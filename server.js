7b3a477a5c048917619a8a283976e8943cb40ff5
Sumit Soni <sumit@Sumits-MacBook-Pro.local>
2018-10-13 18:26:11 -0500

    pushing heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const db = mongoose.connection;
 const Room = require('./models/rooms.js')
 
+app.use(express.urlencoded({extended: false})); //extended: false - does not allow nested objects in query strings
 //PORT
 //allow use of Heroku's port or your own local port, depending on the enviornment
 const PORT = process.env.PORT || 3000
@@ -30,8 +31,6 @@ db.on('open', () => {});
 app.use(express.static('public'));
 
 //populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({extended: false})); //extended: false - does not allow nested objects in query strings
-app.use(express.json()); //returns middleware that only parses json
 
 //Method Override
 app.use(methodOverride('_method')); //allows POST, PUT, and DELETE from a form
@@ -40,28 +39,25 @@ app.use(methodOverride('_method')); //allows POST, PUT, and DELETE from a form
 //localhost:3000 - this will reroute to 'products'
 
 //INDEX
-app.get('/', (req, res)=>{
-  res.send('Hello World')
-})
-// app.get('/budapest', (req, res) => {
-//   Room.find({}, (err, allRooms) => {
-//     res.render('index.ejs', {
-//       rooms:allRooms
-//     })
-//   })
-// });
-
-// //NEW
-// app.get('/budapest/new', (req, res) => {
-//   res.render('new.ejs')
+// app.get('/', (req, res)=>{
+//   res.send('Hello World')
 // })
+app.get('/budapest', (req, res) => {
+    res.render('index.ejs')
+  });
+
+  //CREATE
+  app.post('/budapest', (req, res) => {
+    Room.create(req.body, (err, createdRoom) => {
+      res.redirect('/budapest');
+    })
+  });
+
+//NEW
+app.get('/budapest/new', (req, res) => {
+  res.render('new.ejs')
+})
 //
-// //CREATE
-// app.post('/budapest', (req, res) => {
-//   Room.create(req.body, (err, createdRoom) => {
-//     res.redirect('/budapest');
-//   })
-// });
 // //SHOW
 // app.get('/budapest/:id', (req,res) => {
 //   Room.findById(req.params.id, (err, foundRoom) => {
